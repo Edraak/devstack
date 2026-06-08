b4edd250cac278c4d4050d0e2d776986cb3a7abb
qrawashdeh <qrawashdeh@qrf.org>
2026-06-08 17:28:39 +0300

    Fix provision-edraak-auth.sh file

--- provision-edraak-auth.sh
+++ provision-edraak-auth.sh
@@ -3,14 +3,21 @@ set -e
 
 echo "Provisioning edraak-auth service..."
 
+docker compose `echo ${DOCKER_COMPOSE_FILES}` up -d edraak-auth
+
 docker exec -i edx.devstack.mysql mysql -uroot -e "
     CREATE DATABASE IF NOT EXISTS edraak_auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
     GRANT ALL PRIVILEGES ON edraak_auth.* TO 'root'@'%';
     FLUSH PRIVILEGES;
 "
 
-docker exec -i edraak.devstack.edraak_auth bash -c "
-    python manage.py migrate --noinput
-"
+echo "** edraak-auth: Installing pip dependencies **"
+docker compose `echo ${DOCKER_COMPOSE_FILES}` exec edraak-auth bash -c 'pip install -r requirements.txt'
+
+echo "** edraak-auth: Migrating databases **"
+docker compose `echo ${DOCKER_COMPOSE_FILES}` exec edraak-auth bash -c 'python manage.py migrate --noinput'
+
+echo "** edraak-auth: Restarting **"
+docker compose `echo ${DOCKER_COMPOSE_FILES}` restart edraak-auth
 
 echo "edraak-auth provisioning complete."
